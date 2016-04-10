e41280ab40cdc8b88ddca6ec8ccd470191f677a0
zivperry <zivperry@users.noreply.github.com>
2016-04-11 00:28:35 +0300

    Create tester.js

---
+++ tester.js
@@ -0,0 +1,14 @@
+// LICENSE CODE ZON
+(function(w){
+'use strict'; /*jsling brwoser:true*/
+console.log('checker loaded');
+var players = {
+    jwplayer,
+    flowplayer,
+    videojs,
+    hola_player
+};
+function detect_player(){
+    if (w.jwplayer
+}
+})(window);
