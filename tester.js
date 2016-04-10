138d3fc6fe770981a27b31143498e183d7491d81
zivperry <zivperry@users.noreply.github.com>
2016-04-11 00:41:48 +0300

    Update tester.js

--- tester.js
+++ tester.js
@@ -2,24 +2,36 @@
 (function(w){
 'use strict'; /*jsling brwoser:true*/
 console.log('checker loaded');
-
+var players = {
+    jwplayer: {
+        name: 'jwplayer',
+        version: function(){
+            return w.jwplayer().version;
+        }
+    },
+    flowplayer: {},
+    videojs: {},
+    hola: {},
+    native: {}
+};
 function detect_player(){
     if (w.jwplayer)
-    {
-        console.log('jwplayer');
-    }
+        return players.jwplayer;
     else if (w.flowplayer)
-    {
-        console.log('flowplayer');
-    }
+        return players.flowplayer;
     else if (w.videojs)
-    {
-        console.log('videojs');
-    }
+        return players.videojs;
     else if (w.hola_player)
+        return players.hola;
+    else
     {
-        console.log('hola_player');
+        console.log('implement look for native');
     }
 }
-detect_player();
+function main(){
+    var player = detect_player();
+    
+    console.log(player.name, player.version())
+}
+main();
 })(window);
