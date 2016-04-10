1c6e0404d30a658e0c9d3a46eef20e079c3fef8e
zivperry <zivperry@users.noreply.github.com>
2016-04-11 00:33:45 +0300

    Update tester.js

--- tester.js
+++ tester.js
@@ -2,12 +2,7 @@
 (function(w){
 'use strict'; /*jsling brwoser:true*/
 console.log('checker loaded');
-var players = {
-    jwplayer,
-    flowplayer,
-    videojs,
-    hola_player
-};
+
 function detect_player(){
     if (w.jwplayer)
     {
